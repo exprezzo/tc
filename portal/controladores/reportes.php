bbf35c1736d7bcd308beded34ba6c47d4fd89d60
colab_01.diem <runtim3.error@gmail.com>
2013-08-05 15:39:28 -0600

    reporte de comisiones

--- portal/controladores/reportes.php
+++ portal/controladores/reportes.php
@@ -9,6 +9,113 @@ require_once $APPS_PATH.$_PETICION->modulo.'/vistas/reportes/blanco_pdf.php';
 require_once $APPS_PATH.$_PETICION->modulo.'/modelos/tienda_modelo.php';
 
 class Reportes extends Controlador{
+	function comisionesJson($imprimir=true){			
+		if ( empty($_SESSION) || empty($_SESSION['isLoged']) || !( $_SESSION['userInfo']['rol']==1 || $_SESSION['userInfo']['rol']==2 || $_SESSION['userInfo']['rol']==3)  ) {
+			echo 'No tiene suficientes privilegios para ver este reporte'; exit;
+		}
+		
+		// $fechai=DateTime::createFromFormat ( 'd/m/Y' , $_REQUEST['fechai'] );
+		// $fechaf=DateTime::createFromFormat ( 'd/m/Y' , $_REQUEST['fechaf'] );
+		
+		$fechai = explode('/', $_REQUEST['paging']['fechai']);
+		$fechaf = explode('/', $_REQUEST['paging']['fechaf']);
+		
+		// $agrupar = ($_REQUEST['paging']['agrupar'] =='true') ? true: false;
+		$tienda = $_REQUEST['paging']['tienda'];		
+		$articulo = empty($_REQUEST['paging']['articulo'])? '' : $_REQUEST['paging']['articulo'];		
+		
+		// $filtroArticulo = empty($articulo) ?  ' ' : ' tkd.descripcion LIKE "%'.$articulo.'%" AND ' ;		
+		$filtroTienda =  empty($tienda)? ' ' : ' tkd.tienda= '.$tienda.' AND ';		
+		
+		$start = ($_REQUEST['paging']['pageIndex'] ) * $_REQUEST['paging']['pageSize'];		
+		$limit = $_REQUEST['paging']['pageSize'];		
+		
+		$modelo=new Modelo();
+		$pdo=$modelo->getPdo();									
+		
+		$sql='SELECT  count( distinct concat(tkd.tienda,"-",ti.vendedor) ) as total
+		FROM tick_det tkd 
+		LEFT JOIN tiendas t ON t.clave = tkd.tienda
+		LEFT JOIN tick_int ti ON ti.clave = tkd.clave AND ti.tienda = tkd.tienda
+		LEFT JOIN vendedores v ON v.clave = ti.vendedor AND v.tienda = tkd.tienda
+		WHERE '.$filtroTienda.'  tkd.fechaventa >= "'.$fechai[2].'-'.$fechai[1].'-'.$fechai[0].' 00:00:00" and tkd.fechaventa <= "'.$fechaf[2].'-'.$fechaf[1].'-'.$fechaf[0].' 23:59:59" 
+		ORDER BY  t.tienda ASC ';
+				
+		// echo $sql; exit;
+		$sth = $pdo->query( $sql );
+				
+		if ( !$sth ) {
+			$res=$modelo->getError(  );
+			print_r( $res );
+			exit;
+		}
+		
+		$datos=$sth->fetchAll(PDO::FETCH_ASSOC);
+		$total=$datos[0]['total'];		
+		
+		$sql='SELECT  t.tienda as nombreTienda, concat(tkd.tienda,"-",ti.vendedor) as vendedor_tienda, concat( ifnull(v.nombre, "" ),"(",ti.vendedor,")") as vendedor_nombre,
+		SUM(tkd.cantidad * tkd.precioiva) as  importe, SUM( if (  SUBSTRING(apartado,1,1)="A", tkd.cantidad * tkd.precioiva, 0 ))  as apartado,
+		SUM( if (SUBSTRING(apartado,1,1)="A", 0, tkd.cantidad * tkd.precioiva ))  as noapartado
+		FROM tick_det tkd 
+		LEFT JOIN tiendas t ON t.clave = tkd.tienda
+		LEFT JOIN tick_int ti ON ti.clave = tkd.clave AND ti.tienda = tkd.tienda
+		LEFT JOIN vendedores v ON v.clave = ti.vendedor AND v.tienda = tkd.tienda
+		WHERE '.$filtroTienda.' tkd.fechaventa >= "'.$fechai[2].'-'.$fechai[1].'-'.$fechai[0].' 00:00:00" and tkd.fechaventa <= "'.$fechaf[2].'-'.$fechaf[1].'-'.$fechaf[0].' 23:59:59" 
+		GROUP BY vendedor_tienda ORDER BY  t.tienda ASC LIMIT '.$start.', '.$limit;
+		
+		// echo $sql; exit;
+		
+		$sth = $pdo->query( $sql );
+				
+		if ( !$sth ) {
+			$res=$modelo->getError(  );
+			print_r( $res );
+			exit;
+		}
+		
+		$res=array(
+			'success'	=> true,
+			'datos'		=> $sth->fetchAll(PDO::FETCH_ASSOC)			
+		);	
+		
+		if ($imprimir){
+			$resp = array(
+				'success'=>$res['success'],
+				// 'msg'=>$res['msg'],
+				'totalRows'=>$total,
+				'rows'=>$res['datos']				
+			);
+			echo json_encode( $resp );
+		}
+		return $res;
+	}
+	function comisiones(){
+		$tiendaMod = new TiendaModelo();
+		$res  =$tiendaMod->buscar( array() );				
+		$vista=$this->getVista();
+		$vista->tiendas = $res['datos'];
+		
+		// ------------------
+		// $elMes=date('m');
+		// $dia=date('d');		
+		// $elAnio=date('Y');
+		
+		
+		 // $_REQUEST['paging']['fechai'] = $dia.'/'.$elMes.'/'.$elAnio; 
+		 // $_REQUEST['paging']['fechaf'] = $dia.'/'.$elMes.'/'.$elAnio; 
+		 // $_REQUEST['paging']['agrupar']='true';
+		 // $_REQUEST['paging']['tienda']='';
+		 // $_REQUEST['paging']['pageIndex']=1;
+		 // $_REQUEST['paging']['pageSize']=13;
+		// ------------------
+		
+		// $datos = $this->vendidosJson(false);
+		// $vista->datos = $datos['datos'];
+		// $vista->apartados = $datos['apartados'];
+		// $vista->vendidos = $datos['vendidos'];
+		// $vista->importe = $datos['importe'];
+		$this->mostrarVista();
+	}
 	
 	function novendidos(){
 		
--- portal/vistas/reportes/menu.php
+++ portal/vistas/reportes/menu.php
@@ -21,11 +21,12 @@
 </style>
 <div style="padding:10px;" class="menuReportes">	
 	<div>
-	<h1>Reportes</h1>
-		<a tablink="true" href="<?php echo $_APP_PATH.'portal'; ?>/reportes/vendidos" titulo="Vendidos">
+	<h1>Reportes</h1>		
+		<a tablink="true" href="<?php echo $_APP_PATH.'portal'; ?>/reportes/vendidoshtml" titulo="Vendidos">
 			<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/bluestyle_09_star.png">		 
 			<div>Vendidos</div>
 		</a>
+		
 		<a tablink="true" href="<?php echo $_APP_PATH.'portal'; ?>/reportes/top20" titulo="Top 20">
 			<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/greenstyle_09_star.png">		 
 			<div>Top 20</div>
@@ -39,11 +40,17 @@
 			<div>No vendidos</div>
 		</a>
 	</div>
+	
 	<div>
-		<a tablink="true" href="<?php echo $_APP_PATH.'portal'; ?>/reportes/vendidoshtml" titulo="Vendidos">
-			<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/bluestyle_09_star.png">		 
-			<div>Vendidos Html</div>
+		<a tablink="true" href="<?php echo $_APP_PATH.'portal'; ?>/reportes/comisiones" titulo="Comisiones">
+			<img src="http://png.findicons.com/files/icons/190/business_toolbar/48/payment.png">
+			<div>Comisiones</div>
 		</a>
+		<a tablink="true" href="<?php echo $_APP_PATH.'portal'; ?>/reportes/cortes" titulo="Cortes de caja">
+			<img src="http://png.findicons.com/files/icons/192/finance/64/cash_register.png">
+			<div>Cortes de caja</div>
+		</a>
+		
 	</div>
 </div>
 
